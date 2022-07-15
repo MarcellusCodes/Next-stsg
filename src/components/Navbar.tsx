908aed69bc5adc5547e670ee2ca499880fbfc5f8
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-15 13:43:16 +0200

    Changed Padding on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Branding, PrimaryButton } from "./index";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white py-4 px-4">
+    <nav className="bg-white py-2 px-4">
       <div className="flex flex-row items-center container mx-auto justify-between">
         <Branding />
         <PrimaryButton
