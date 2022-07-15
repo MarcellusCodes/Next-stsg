d03a4f48203ea5a910445d3d92b2a965abaed2b3
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-15 13:09:02 +0200

    Added onClick handle to Button

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 interface IPrimaryButton {
   children: React.ReactNode;
+  onClick: () => void;
 }
 
-const PrimaryButton: React.FC<IPrimaryButton> = ({ children }) => {
+const PrimaryButton: React.FC<IPrimaryButton> = ({ children, onClick }) => {
   return (
-    <button className="bg-primary-500 hover:bg-primary-300 active:bg-primary-700 text-slate-50 px-6 py-2 text-xl font-primary rounded-md">
+    <button
+      onClick={onClick}
+      className="bg-primary-500 hover:bg-primary-300 active:bg-primary-700 text-slate-50 px-6 py-2 text-xl font-primary rounded-md"
+    >
       {children}
     </button>
   );
