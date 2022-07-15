6952aa05fb83bd5a1f115fbfc554a3abe33d1576
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-15 13:02:02 +0200

    Added Shades for Primary and Secondary Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#4e54c8",
-        secondary: "#8f94fb",
+        primary: {
+          50: "#DADBF4",
+          100: "#CBCCEF",
+          200: "#ABAEE5",
+          300: "#8C90DB",
+          400: "#6D72D2",
+          500: "#4E54C8",
+          600: "#353AA9",
+          700: "#272C7E",
+          800: "#1A1D54",
+          900: "#0D0E29",
+        },
+        secondary: {
+          50: "#FFFFFF",
+          100: "#FFFFFF",
+          200: "#FFFFFF",
+          300: "#DEDFFE",
+          400: "#B6BAFC",
+          500: "#8F94FB",
+          600: "#5960F9",
+          700: "#232DF7",
+          800: "#0812DA",
+          900: "#060DA4",
+        },
       },
       fontFamily: {
         primary: ["Marvel", "sans-serif"],
