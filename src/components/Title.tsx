dad9a4bac010ccd4bbc3e83b6b1f8267ad647814
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-21 15:38:04 +0200

    Added h2 Tag as reusable title tag, h1 only used in header

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -9,7 +9,7 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ color, title }) => {
   return (
-    <motion.h1
+    <motion.h2
       initial={{
         y: -50,
         clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)",
@@ -22,10 +22,10 @@ const Title: React.FC<TitleProps> = ({ color, title }) => {
       }}
       viewport={{ once: true, margin: "0px 0px -200px 0px" }}
       transition={{ duration: 1, ...Easing }}
-      className={`font-primary ${color} text-4xl sm:text-5xl lg:text-6xl font-bold`}
+      className={`font-primary text-primary-500 text-3xl sm:text-4xl lg:text-5xl font-bold`}
     >
       {title}
-    </motion.h1>
+    </motion.h2>
   );
 };
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,3 +4,4 @@ export { default as PrimaryButton } from "./PrimaryButton";
 export { default as Header } from "./Header";
 export { default as Info } from "./Info";
 export { default as Title } from "./Title";
+export { default as Heading } from "./Heading";
