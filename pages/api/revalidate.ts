2a053373b7f472b5682e960bdf2f32e0427565bb
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-22 13:48:46 +0200

    Added SSG for Battles, ISR

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,41 @@
+import { isValidRequest } from "@sanity/webhook";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  message: string;
+};
+
+const secret = process.env.SANITY_SECRET_TOKEN;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "POST") {
+    console.error("Must be a POST request");
+    return res.status(401).json({ message: "Must be a POST request" });
+  }
+
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: "Invalid signature" });
+    return;
+  }
+
+  try {
+    const {
+      body: { type, battleId },
+    } = req;
+
+    switch (type) {
+      case "battle":
+        await res.revalidate(`/battle/${battleId}`);
+        return res.json({
+          message: `Revalidated "${type}" with slug "${battleId}"`,
+        });
+    }
+
+    return res.json({ message: "No managed type" });
+  } catch (err) {
+    return res.status(500).send({ message: "Error revalidating" });
+  }
+}
