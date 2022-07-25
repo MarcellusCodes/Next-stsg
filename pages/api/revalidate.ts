03f431ddad4a5b81a642b5f51c71489fa2b40da5
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-25 15:59:18 +0200

    Test Dynamic values from webhook

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -16,21 +16,19 @@ export default async function handler(
     return res.status(401).json({ message: "Must be a POST request" });
   }
 
+  const { battleId } = await req.body;
+
   /*if (!isValidRequest(req, secret)) {
     res.status(401).json({ message: "Invalid signature" });
     return;
   }*/
 
   try {
-    const type = "battle";
-    const battleId = "aaed8b4e-7b9f-4d4d-8a38-c57ca57d3204";
-    switch (type) {
-      case "battle":
-        await res.revalidate(`/battle/${battleId}`);
-        return res.json({
-          message: `Revalidated "${type}" with slug "${battleId}"`,
-        });
-    }
+    const battleid = "aaed8b4e-7b9f-4d4d-8a38-c57ca57d3204";
+    await res.revalidate(`/battle/${battleid}`);
+    return res.json({
+      message: `Revalidated battle: "${battleId}"`,
+    });
   } catch (err) {
     return res.status(500).send({ message: "Error revalidating" });
   }
