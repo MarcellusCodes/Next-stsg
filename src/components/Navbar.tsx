c7bbe1e109acef1acda4c287afbe296d725ee0b2
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-26 10:17:38 +0200

    Added children Links for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Branding, PrimaryButton } from "./index";
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+  children: React.ReactNode;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ children }) => {
   return (
     <nav className="bg-white py-2 px-4">
       <div className="flex flex-row items-center container mx-auto justify-between">
         <Branding />
-        <PrimaryButton
-          onClick={() => {
-            console.log("Login");
-          }}
-        >
-          Login
-        </PrimaryButton>
+        <ul className="flex flex-row items-center space-x-6">
+          {children}
+          <li>
+            <PrimaryButton
+              onClick={() => {
+                console.log("Login");
+              }}
+            >
+              Login
+            </PrimaryButton>
+          </li>
+        </ul>
       </div>
     </nav>
   );
