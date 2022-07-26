6a0cc8f448f51ac303825d159e9c4f009cfcd249
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-26 10:20:23 +0200

    Added Link for going back to Landing page on Branding

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Branding = () => {
   return (
-    <div className="grid place-content-center space-x-2 border-2 rounded-full border-primary-500 w-16 h-16">
-      <h2 className="text-4xl text-primary-500 font-primary">SB</h2>
-    </div>
+    <Link href="/" passHref>
+      <div className="grid place-content-center space-x-2 border-2 rounded-full border-primary-500 w-16 h-16 cursor-pointer">
+        <h2 className="text-4xl text-primary-500 font-primary">SB</h2>
+      </div>
+    </Link>
   );
 };
 
